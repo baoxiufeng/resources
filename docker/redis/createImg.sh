b560d5b4acbe0eb28f1052e077c997accbed5edd
baoxiufeng <bxf_open@126.com>
2017-12-25 07:39:12 +0000

    delete unused files

--- docker/redis/createImg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for port in 6379 6380 6381 6389 6390 6391
-do
-  echo "Start to create docker image for redis_$port"
-  ln -sf dockerfiles/dockerfile_$port dockerfile
-  docker build -t bxf/redis_$port .
-done
