d74d4c41f09fec22186edf6a2c9f77847033a105
baoxiufeng <bxf_open@126.com>
2017-12-25 04:22:07 +0000

    To update the redis docker image create and start shell scripts

--- docker/redis/createImg.sh
+++ docker/redis/createImg.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo "Start to create docker image for redis:$1"
-
-ln -sf dockerfiles/dockerfile_$1 dockerfile
-docker build -t bxf/redis_$1 .
+for port in 6379 6380 6381 6389 6390 6391
+do
+  echo "Start to create docker image for redis_$port"
+  ln -sf dockerfiles/dockerfile_$port dockerfile
+  docker build -t bxf/redis_$port .
+done
--- docker/redis/startImg.sh
+++ docker/redis/startImg.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker run -d --name redis_$1 --net=host bxf/redis_$1
+for port in 6379 6380 6381 6389 6390 6391
+do
+  echo "To run docker: bxf/redis_$port"
+  docker run -d --name redis_$port --net=host bxf/redis_$port
+done
