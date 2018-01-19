894704874fe5b5c5fc2d99f0e7edbd0baaba040e
baoxiufeng <bxf_open@126.com>
2018-01-19 20:06:21 +0800

    delete unused files

--- docker/middleware/redis/createRubyRedisImg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#author:baoxiufeng
-
-echo "Start to create redis cluster tool docker image..."
-cd ruby
-docker build -t bxf/ruby_redis .
--- docker/middleware/redis/startAllRedisImg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#author:baoxiufeng
-
-for port in 6379 6380 6381 6389 6390 6391
-do
-  echo "To run docker: bxf/redis_$port"
-  docker run -d --name redis_$port --net=host bxf/redis_$port
-done
