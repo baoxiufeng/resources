4c82ec526f36c15f56d494bb2238412026a2a029
baoxiufeng <bxf_open@126.com>
2018-08-24 16:30:48 +0800

    update start elasticsearch shell

--- docker/middleware/elasticsearch/startCluster.sh
+++ docker/middleware/elasticsearch/startCluster.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-docker run -d --privileged --name $2 --net=host bxf/elasticsearch$1 sh -c "su - elasticsearch -c 'startCluster.sh $2 $3 $4'"
+#docker run -d --privileged --name $2 --net=host bxf/elasticsearch$1 sh -c "su - elasticsearch -c 'startCluster.sh $2 $3 $4'"
+docker run -d --privileged --name $1 --net=host bxf/elasticsearch sh -c "su - elasticsearch -c 'sh /opt/es/startEs.sh $1 $2 $3'"
 docker ps -a
